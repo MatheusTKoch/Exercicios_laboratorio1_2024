89c65b1c35696852ba0c50b83823df50d155f052
Matheus Trilha Koch <matheust_koch@hotmail.com>
2024-05-18 22:17:30 -0300

    Terminando at 4

--- Exercicio4Lab/Exerc4Lab/src/Empresa.java
+++ Exercicio4Lab/Exerc4Lab/src/Empresa.java
@@ -12,4 +12,54 @@ public class Empresa {
     private String nome;
     private String funcionario1;
     private String funcionario2;
+
+    public Empresa(String nome, String funcionario1, String funcionario2) {
+        this.nome = nome;
+        this.funcionario1 = funcionario1;
+        this.funcionario2 = funcionario2;
+    }
+
+    public Empresa(String nome) {
+        this.nome = nome;
+        this.funcionario1 = null;
+        this.funcionario2 = null;
+    }
+
+    public Empresa() {
+        this.nome = "Teste";
+        this.funcionario1 = null;
+        this.funcionario2 = null;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getFuncionario1() {
+        return funcionario1;
+    }
+
+    public String getFuncionario2() {
+        return funcionario2;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setFuncionario1(String funcionario1) {
+        this.funcionario1 = funcionario1;
+    }
+
+    public void setFuncionario2(String funcionario2) {
+        this.funcionario2 = funcionario2;
+    }
+
+    public void imprimeInfo() {
+        System.out.println("Nome da Empresa" + nome);
+        System.out.println("Funcionario1" + funcionario1);
+        System.out.println("Funcionario2" + funcionario2);
+    }
+
+
 }
