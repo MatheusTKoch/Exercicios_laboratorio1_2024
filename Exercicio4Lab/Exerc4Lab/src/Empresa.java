a1ffbf8ff55a86b70dac79b1bede12f65a414212
Matheus Trilha Koch <matheust_koch@hotmail.com>
2024-05-18 21:42:20 -0300

    Teste

---
+++ Exercicio4Lab/Exerc4Lab/src/Empresa.java
@@ -0,0 +1,15 @@
+/**
+Classe Empresa:
+- Uma empresa tem um nome e dois funcionários do caixa;
+- Crie três construtores: um que recebe todos os parâmetros para inicializar os atributos; outro que recebe apenas
+o nome da empresa, inicializando os funcionários em null; outro que não recebe parâmetros, inicializando os
+funcionários em null e o nome com algum valor padrão.
+- Crie os métodos de acesso dos atributos desta classe (get e set);
+- Crie um método “imprimeInfo”, que imprime as informações da classe. 
+ */
+
+public class Empresa {
+    private String nome;
+    private String funcionario1;
+    private String funcionario2;
+}
