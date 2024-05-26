3345bf5c8650966400cf40805ca9061d38335bc6
Matheus <matheustrilhakoch@hotmail.com>
2024-05-26 10:52:53 -0300

    Iniciando classe de teste

---
+++ Exercicio5Lab/Exerc5/src/MainTeste.java
@@ -0,0 +1,10 @@
+public class MainTeste {
+    public static void main(String[] args) {
+        Banana caturraBanana = new Banana("Caturra", 5.9, "Madura");
+        Melancia mediaMelancia = new Melancia("Melancia Media", 15, 8, false);
+        Programador euProgramador = new Programador("Matheus", 25, "Javascript");
+        Aluno euAluno = new Aluno("Matheus", 25, 8.5);
+
+        
+    }
+}
