da1587a8bad66b460a5d77b55afbf95002bd019a
Matheus <matheustrilhakoch@hotmail.com>
2024-05-26 13:46:42 -0300

    Finalizando classe main

--- Exercicio5Lab/Exerc5/src/MainTeste.java
+++ Exercicio5Lab/Exerc5/src/MainTeste.java
@@ -6,6 +6,7 @@ public class MainTeste {
         Melancia mediaMelancia = new Melancia("Melancia Media", 15, 8, false);
         Programador euProgramador = new Programador("Matheus", 25, "Javascript");
         Aluno euAluno = new Aluno("Matheus", 25, 8.5);
+        boolean finalizar = false;
 
         Scanner scanner = new Scanner(System.in);
 
@@ -16,17 +17,28 @@ public class MainTeste {
         int idadeP = scanner.nextInt();
         Pessoa p = new Pessoa(nomeP, idadeP);
         System.out.println("Digite 1 para programador ou 2 para aluno");
-        while (scanner.hasNextInt()) {
+        if (scanner.hasNextInt()) {
             int opcao = scanner.nextInt();
             if (opcao == 1) {
-                System.out.println("Dgite sua linguagem de programacao favorita: ");
+                System.out.println("Digite sua linguagem de programacao favorita: ");
+                String linguagem = scanner.next();
+                Programador p1 = new Programador(p.getNome(), p.getIdade(), linguagem);
+                System.out.println("Ola " + p1.getNome() + "!");
+                System.out.print("Voce tem " + p1.getIdade() + " anos, e sua linguagem favorita: " + p1.getLinguagem());
+                finalizar = true;
             } else if (opcao == 2) {
                 System.out.println("Digite a nota recebida: ");
+                double nota = scanner.nextDouble();
+                Aluno a1 = new Aluno(p.getNome(), p.getIdade(), nota);
+                System.out.println("Ola " + a1.getNome() + "!");
+                System.out.print("Voce tem " + a1.getIdade() + " anos, e essa e sua nota: " + a1.getNota());
+                finalizar = true;
             } else {
                 System.out.println("Opcao Incorreta");
             }
         }
-        
-        scanner.close();
+        if (finalizar == true) {
+            scanner.close();
+        }    
     }
 }
