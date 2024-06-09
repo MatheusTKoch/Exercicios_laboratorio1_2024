1028b5ca45182402b95526433ffc9e25223ef5c0
Matheus <matheustrilhakoch@hotmail.com>
2024-06-08 21:31:05 -0300

    Testando exercicios

--- Exercicio7Lab/Exerc7/src/Exercicio5.java
+++ Exercicio7Lab/Exerc7/src/Exercicio5.java
@@ -1,6 +1,7 @@
 public class Exercicio5 {
     public static void main(String[] args) {
         int[] a = {507, -15, 147, 2194, 300, 27, 888, -110, 0, 675};
-        System.out.println(a[7] + 2);
+        double[] valores = null;
+        System.out.println(a[a.length-1]);
     }
 }
