9f27616c4426d75f09bcf432d11b2c17abae01e1
MatheusTKoch <matheust_koch@hotmail.com>
2024-06-25 20:48:55 -0300

    Adicionando verificalivro

---
+++ Exercicio8Lab/Exerc8/src/Exercicio1.java
@@ -0,0 +1,9 @@
+public class Exercicio1 {
+    private Livro[] arrayLivros;
+    public String titulo;
+
+    public Exercicio1(Livro[] arrayLivros, String titulo) {
+        this.arrayLivros =  arrayLivros;
+        this.titulo = titulo;
+    }
+}
