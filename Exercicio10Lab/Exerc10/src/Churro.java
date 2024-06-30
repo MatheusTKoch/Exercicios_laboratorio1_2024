c8fc637bd05e111cdf621c50888df75d90fee219
Matheus <matheustrilhakoch@hotmail.com>
2024-06-29 22:53:20 -0300

    Criado met de acesso e toString

--- Exercicio10Lab/Exerc10/src/Churro.java
+++ Exercicio10Lab/Exerc10/src/Churro.java
@@ -19,5 +19,23 @@ public class Churro {
         this.preco = 9;
     }
 
-    
+    public String getSabor() {
+        return sabor;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String toString() {
+        return "Sabor do churros: " + getSabor() + ", Preco: " + getPreco() + "R$";
+    }
 }
